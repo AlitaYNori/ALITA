277e84115bb9dc740aa67fd33c45a4eadc284e7e
AlitaYNori <baezalita0@gmail.com>
2026-07-07 23:08:00 -0300

    packages.sh

---
+++ packages.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+sudo pacman -S --needed \
+hyprland \
+waybar \
+rofi \
+kitty \
+dolphin \
+hyprpaper \
+networkmanager \
+network-manager-applet \
+pipewire \
+wireplumber \
+pavucontrol \
+grim \
+slurp \
+wl-clipboard \
+brightnessctl \
+playerctl \
+ttf-jetbrains-mono-nerd \
+ttf-firacode-nerd
+
+echo "Paquetes base instalados."
